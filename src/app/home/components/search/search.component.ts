72fcc70669b197c36626df287199bb72e8583099
Lennart97 <45463934+Lennart97@users.noreply.github.com>
2022-04-10 16:32:21 +0200

    Close autocomplete on document clicked

--- src/app/home/components/search/search.component.ts
+++ src/app/home/components/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { merge, Observable } from 'rxjs';
+import { merge, Observable, Subject } from 'rxjs';
 import { debounceTime, filter, map } from 'rxjs/operators';
 import { FocusMonitor } from '@angular/cdk/a11y';
 import { MatInput } from '@angular/material/input';
@@ -25,6 +25,7 @@ export class SearchComponent implements OnInit {
   @ViewChild(MatInput, { read: ElementRef, static: true }) private inputEl!: ElementRef;
   @ViewChild(CdkConnectedOverlay, { static: true }) private connectedOverlay!: CdkConnectedOverlay;
   searchFormControl = new FormControl();
+  manualClose = new Subject<void>();
 
   constructor(
     private focusMonitor: FocusMonitor,
@@ -33,13 +34,14 @@ export class SearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const manualClose$ = this.manualClose.pipe(map(() => false));
     const searchStarted$ = this.searchSubmit.pipe(map(() => false));
     this.isPanelHidden$ = this.connectedOverlay.backdropClick.pipe(map(() => false));
     this.isPanelVisible$ = this.focusMonitor.monitor(this.inputEl).pipe(
       filter(focused => !!focused),
       map(() => true)
     );
-    this.showPanel$ = merge(this.isPanelHidden$, this.isPanelVisible$, searchStarted$);
+    this.showPanel$ = merge(this.isPanelHidden$, this.isPanelVisible$, searchStarted$, manualClose$);
   }
 
   search() {
@@ -48,5 +50,9 @@ export class SearchComponent implements OnInit {
 
   onAssetClicked(documentId: string | number, assetId: string | number) {
     window.open(environment.serverConfig.root + '/documents/' + documentId + '/assets/' + assetId, 'blank');
+  }
+  
+  onDocumentClicked() {
+    this.manualClose.next();
   }
 }
